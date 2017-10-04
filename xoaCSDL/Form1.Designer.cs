0697279c4968d96b03b107f4c97e4b0a221dfe47
Your Name <vothanhhai1996@gmail.com>
2017-10-04 11:11:27 +0700

    lan 1

---
+++ xoaCSDL/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace xoaCSDL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTenLoai = new System.Windows.Forms.TextBox();
+            this.txtMaLoai = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMaLoai);
+            this.groupBox1.Controls.Add(this.txtTenLoai);
+            this.groupBox1.Location = new System.Drawing.Point(79, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại sản phẩm";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dtgv);
+            this.groupBox2.Controls.Add(this.btDelete);
+            this.groupBox2.Location = new System.Drawing.Point(79, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(355, 180);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtTenLoai
+            // 
+            this.txtTenLoai.Location = new System.Drawing.Point(168, 74);
+            this.txtTenLoai.Name = "txtTenLoai";
+            this.txtTenLoai.Size = new System.Drawing.Size(149, 20);
+            this.txtTenLoai.TabIndex = 0;
+            // 
+            // txtMaLoai
+            // 
+            this.txtMaLoai.Location = new System.Drawing.Point(168, 48);
+            this.txtMaLoai.Name = "txtMaLoai";
+            this.txtMaLoai.Size = new System.Drawing.Size(149, 20);
+            this.txtMaLoai.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã Loại";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên loại";
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(126, 19);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(98, 26);
+            this.btDelete.TabIndex = 0;
+            this.btDelete.Text = "Xóa";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // dtgv
+            // 
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Location = new System.Drawing.Point(19, 44);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.Size = new System.Drawing.Size(330, 130);
+            this.dtgv.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 340);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Loai san pham";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaLoai;
+        private System.Windows.Forms.TextBox txtTenLoai;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.DataGridView dtgv;
+    }
+}
+
---
+++ xoaCSDL/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace xoaCSDL
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection cn=null;// khai báo
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // chuối kết nối
+            string cnstr = "Server=. ;Database=QLBanHang;Integrated security=true;";
+            cn = new SqlConnection(cnstr);
+            dtgv.DataSource = GetData();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        public void connect() {
+            try
+            {
+                if (cn != null &&  cn.State != ConnectionState.Open) {
+                    cn.Open();
+                    
+                }
+
+
+            }
+            catch (InvalidOperationException ex)
+            {
+
+                MessageBox.Show("không thể kết nối"+ ex.Message);
+            }
+            
+        }
+        //đóng kết nối
+        public void disConnect() {
+            if (cn != null && cn.State != ConnectionState.Closed)
+                cn.Close();
+        }
+        private List<Object>GetData(){
+            connect();
+            string sql = "select * from LoaiSP";
+            List<Object> list = new List<object>();
+            try
+            {
+                // dùng đối tượng sql Command
+                SqlCommand cm = new SqlCommand(sql,cn);
+                SqlDataReader dr = cm.ExecuteReader();
+                // khai báo các kiểu dữ liệu trong csdl
+                string  ten;
+              
+                int ma;
+                // bắt đầu đọc từ csdl lên grv
+                while(dr.Read()){
+                    // bắt đầu lấy dữ liệu từng cột
+                    ma = dr.GetInt32(0);
+                    ten = dr.GetString(1);
+                   
+                    //
+                    var prod = new
+                    {
+                        
+                        TenSP = ten,
+                        maLoai = ma
+                    };
+                    list.Add(prod);
+
+                }
+                
+                dr.Close();
+                
+
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+            finally{
+                disConnect();
+            }
+            return list;
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            string query = "Delete from LoaiSP where MaLoaiSp = " + txtMaLoai.Text.ToString();
+            SqlCommand cmd = new SqlCommand(query,cn);
+            int NumberOfRows = 0;
+            connect();
+            NumberOfRows = cmd.ExecuteNonQuery();
+            MessageBox.Show("Số dòng đã xóa là "+ NumberOfRows.ToString());
+            disConnect();
+            dtgv.DataSource = GetData();
+
+        }
+
+    }
+}
